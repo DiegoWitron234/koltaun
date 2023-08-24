83070165b591b81fad319af12dfa1ff39b9bc450
DiegoWitron234 <maximusdiegoalonsodbz@gmail.com>
2023-08-24 14:09:51 -0600

    Versión casi terminada, falta agregar fotos y colaboradores en Acerca De

--- app/src/main/java/aplicacion/principal/AcercaDe.kt
+++ app/src/main/java/aplicacion/principal/AcercaDe.kt
@@ -1,11 +1,27 @@
 package aplicacion.principal
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class AcercaDe : AppCompatActivity() {
+
+    private lateinit var txtAvisoPrivacidad: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_acerca_de)
+
+        txtAvisoPrivacidad = findViewById(R.id.txtAvisoPrivacidad)
+
+        txtAvisoPrivacidad.setOnClickListener {
+            openHyperlink("https://drive.google.com/file/d/1gvOjSH_-BYWvEoIGlE6eS6BLOy_tmnY8/view")
+        }
+    }
+
+    private fun openHyperlink(url: String) {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        startActivity(intent)
     }
 }
